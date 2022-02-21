cab89db7303f5cf18a95fa8b3fc9b2430778567a
Miks 'laame' Ramāns <63640344+Pukkah@users.noreply.github.com>
2022-02-22 00:11:13 +0200

    Add files via upload

---
+++ src/main/java/io/codelex/DayOne.java
@@ -0,0 +1,23 @@
+package io.codelex;
+
+import java.util.Scanner;
+
+public class DayOne {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = input.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = input.nextInt();
+
+        System.out.println("Sum of " + a + " and " + b + " is: " + (a + b));
+
+        System.out.print("Enter your name: ");
+        String name = input.next();
+
+        System.out.println("Good bye, " + name + "!\nSee You next time.");
+    }
+}
