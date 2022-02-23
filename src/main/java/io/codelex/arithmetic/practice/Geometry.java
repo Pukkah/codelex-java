2cc4724134bd5a61b63bd0cb393a012de3b26a77
Pukkah <miksramans@gmail.com>
2022-02-24 01:14:27 +0200

    Completed arithmetic

--- src/main/java/io/codelex/arithmetic/practice/Geometry.java
+++ src/main/java/io/codelex/arithmetic/practice/Geometry.java
@@ -1,19 +1,33 @@
 package io.codelex.arithmetic.practice;
 
-import io.codelex.NoCodeWrittenException;
-
 import java.math.BigDecimal;
 
 class Geometry {
     static double areaOfCircle(BigDecimal radius) {
-        throw new NoCodeWrittenException();
+        radius = fixIfNegative(radius);
+        BigDecimal pi = new BigDecimal(Math.PI);
+        BigDecimal area = pi.multiply(radius).multiply(new BigDecimal(2));
+        return area.doubleValue();
     }
 
     static double areaOfRectangle(BigDecimal length, BigDecimal width) {
-        throw new NoCodeWrittenException();
+        length = fixIfNegative(length);
+        width = fixIfNegative(width);
+        return length.multiply(width).doubleValue();
     }
 
-    static double areaOfTriangle(BigDecimal base, BigDecimal h) {
-        throw new NoCodeWrittenException();
+    static double areaOfTriangle(BigDecimal base, BigDecimal height) {
+        base = fixIfNegative(base);
+        height = fixIfNegative(height);
+        return base.multiply(height).multiply(BigDecimal.valueOf(0.5)).doubleValue();
+    }
+
+    private static BigDecimal fixIfNegative(BigDecimal value) {
+        if (value.signum() == -1) {
+            System.out.println("Negative value detected!");
+            System.out.println("Assuming it was a mistake..");
+            return value.abs();
+        }
+        return value;
     }
 }
