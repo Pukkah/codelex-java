0e903898596a75f154873c0c548484488872f45f
Pukkah <miksramans@gmail.com>
2022-03-04 18:59:28 +0200

    Hangman update

--- src/main/java/io/codelex/arrays/practice/Exercise8.java
+++ src/main/java/io/codelex/arrays/practice/Exercise8.java
@@ -1,16 +1,19 @@
 package io.codelex.arrays.practice;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 // Hangman
 public class Exercise8 {
 
     public static void main(String[] args) {
+        // Settings and Data
         final String[] WORDS = {"one", "two", "three"};
         final int MAX_MISSES = 3;
 
-        int rand = (int) (WORDS.length * Math.random());
-        char[] board = new char[WORDS[rand].length()];
+        // init
+        final int seed = (int) (WORDS.length * Math.random());
+        char[] word = new char[WORDS[seed].length()];
         char[] misses = new char[MAX_MISSES];
 
         Scanner in = new Scanner(System.in);
@@ -19,14 +22,14 @@ public class Exercise8 {
         while (true) {
 
             // print fancy line
-            for (int i = 0; i < 9 + board.length * 2; i++) {
+            for (int i = 0; i < 9 + word.length * 2; i++) {
                 System.out.print(i % 2 == 0 ? '-' : '=');
             }
             System.out.println();
 
             // print word
             System.out.print("Word:  ");
-            for (char ch : board) {
+            for (char ch : word) {
                 System.out.print(" " + (ch != 0 ? ch : "_"));
             }
             System.out.println();
@@ -43,28 +46,14 @@ public class Exercise8 {
             System.out.println();
 
             // check if win
-            boolean win = true;
-            for (char ch : board) {
-                if (ch == 0) {
-                    win = false;
-                    break;
-                }
-            }
-            if (win) {
+            if (Arrays.equals(word, WORDS[seed].toCharArray())) {
                 System.out.println("You WIN!");
                 break;
             }
 
             //check if lose
-            boolean lose = true;
-            for (char ch : misses) {
-                if (ch == 0) {
-                    lose = false;
-                    break;
-                }
-            }
-            if (lose) {
-                System.out.println("Answer: " + WORDS[rand]);
+            if (misses[MAX_MISSES-1] != 0) {
+                System.out.println("Answer: " + WORDS[seed]);
                 System.out.println("You LOSE!");
                 break;
             }
@@ -75,9 +64,9 @@ public class Exercise8 {
 
             // check if hit and place in board
             var hit = false;
-            for (int i = 0; i < board.length; i++) {
-                if (WORDS[rand].charAt(i) == guess) {
-                    board[i] = guess;
+            for (int i = 0; i < word.length; i++) {
+                if (WORDS[seed].charAt(i) == guess) {
+                    word[i] = guess;
                     hit = true;
                 }
             }
@@ -85,7 +74,7 @@ public class Exercise8 {
             // add miss
             if (!hit) {
                 for (int i = 0; i < MAX_MISSES; i++) {
-                    if (misses[i] == 0) {
+                    if (misses[i] == 0 || misses[i] == guess) {
                         misses[i] = guess;
                         break;
                     }
