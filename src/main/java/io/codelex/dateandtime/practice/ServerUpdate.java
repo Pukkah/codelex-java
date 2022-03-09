e5c52894f29a2fbd3e7268cdd7b5de9433d10062
Pukkah <miksramans@gmail.com>
2022-03-09 22:42:28 +0200

    Completed ServerUpdate

---
+++ src/main/java/io/codelex/dateandtime/practice/ServerUpdate.java
@@ -0,0 +1,33 @@
+package io.codelex.dateandtime.practice;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class ServerUpdate {
+    public static void main(String[] args) {
+        System.out.println("* - Date format 06-04-1988 (date-month-year)");
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd-MM-yyy");
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter Server launch date: ");
+        LocalDate serverLaunched = LocalDate.parse(in.nextLine(), dateFormat);
+
+        System.out.println("Check update schedule!");
+        System.out.print("Year: ");
+        int year = in.nextInt();
+        in.nextLine();
+        System.out.print("Month: ");
+        int month = in.nextInt();
+        LocalDate monthInQuestion = LocalDate.of(year, month, 1);
+
+        LocalDate date = serverLaunched;
+        while (date.isBefore(monthInQuestion.plusMonths(1))) {
+            date = date.plusDays(14);
+            if (date.getYear() == monthInQuestion.getYear() && date.getMonth() == monthInQuestion.getMonth()) {
+                System.out.println(date);
+            }
+        }
+
+    }
+}
