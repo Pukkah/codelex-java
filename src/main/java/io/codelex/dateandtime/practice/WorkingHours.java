71df4f6b8b8f3a40e4cf6e0d819ba97f5cbc3d72
Pukkah <miksramans@gmail.com>
2022-03-09 22:23:31 +0200

    Fixed WorkingHours

--- src/main/java/io/codelex/dateandtime/practice/WorkingHours.java
+++ src/main/java/io/codelex/dateandtime/practice/WorkingHours.java
@@ -2,13 +2,12 @@ package io.codelex.dateandtime.practice;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
-import java.time.Period;
 import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class WorkingHours {
     public static void main(String[] args) {
-        System.out.println("* - Date format 06-04-1988 (date-month-year)");
+        System.out.println("[Date format 06-04-1988 (date-month-year)]");
         DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd-MM-yyy");
 
         Scanner in = new Scanner(System.in);
@@ -17,25 +16,13 @@ public class WorkingHours {
         System.out.print("Enter End date: ");
         LocalDate endDate = LocalDate.parse(in.nextLine(), dateFormat).plusDays(1);
 
-        Period employed = Period.between(startDate, endDate);
-        if (employed.isNegative()) {
-            System.out.println("Is this some \"Back to the Future\" stuff?");
-        } else {
-            int daysWorked = employed.getDays();
-            if (daysWorked < 7) {
-                for (LocalDate date = startDate; date.isBefore(endDate); date = date.plusDays(1)) {
-                    if (date.getDayOfWeek() == DayOfWeek.SATURDAY || date.getDayOfWeek() == DayOfWeek.SUNDAY) {
-                        daysWorked--;
-                    }
-                }
+        int hoursWorked = 0;
+        for (LocalDate date = startDate; date.isBefore(endDate); date = date.plusDays(1)) {
+            if (date.getDayOfWeek() != DayOfWeek.SATURDAY && date.getDayOfWeek() != DayOfWeek.SUNDAY) {
+                hoursWorked += 8;
             }
-            int weeksWorked = daysWorked / 7;
-            int remainingDays = daysWorked % 7;
-            if (remainingDays > 5) { remainingDays = 5; }
-            int hoursWorked = (weeksWorked * 5 + remainingDays) * 8;
-
-            System.out.println("Spent " + hoursWorked + " hours working.");
         }
+        System.out.println("Spent " + hoursWorked + " hours working.");
 
     }
 }
