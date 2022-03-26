7e905591d0f2e01669a709b2711108ff4b632a31
laame <miksramans@gmail.com>
2022-03-26 23:11:28 +0200

    Revert StreamsExerciseTest

--- src/test/java/io/codelex/streams/practice/StreamsExerciseTest.java
+++ src/test/java/io/codelex/streams/practice/StreamsExerciseTest.java
@@ -5,7 +5,6 @@ import org.hamcrest.collection.IsIterableContainingInOrder;
 import org.junit.jupiter.api.Test;
 
 import java.util.*;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
